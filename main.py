493f1f4b63e81c37d28fdbad9e3a9d9e193c9b0a
mrspaghettiiscool <jonahposner@icloud.com>
2022-02-21 17:00:49 -0500

    switched to for loop for blit-ing

--- main.py
+++ main.py
@@ -21,10 +21,8 @@ else:
 
 
 anim = Animate(SCREEN)
-
 check = Check()
 
-letter_list = []
 flip_list = [
 	[Check(), (0, 0)],
 	[Check(), (50, 0)],
@@ -41,14 +39,25 @@ while True:
 	if not KEY_INPUT:
 
 		for x in flip_list:
-			x[0].check_key(''.join(random.choices(string.ascii_letters+string.digits,k=1)), KEY_INPUT)
+			x[0].check_key(''.join(random.choices(string.ascii_lowercase,k=1)), KEY_INPUT)
+			#anim.main(x[1], [x[0].BLIT_IMG])
+			SCREEN.blit(x[0].BLIT_IMG, x[1])
+		pygame.display.update()
 
 
-		SCREEN.blit(flip_list[0][0].BLIT_IMG, flip_list[0][1])
-		SCREEN.blit(flip_list[1][0].BLIT_IMG, flip_list[1][1])
-		SCREEN.blit(flip_list[2][0].BLIT_IMG, flip_list[2][1])
-		SCREEN.blit(flip_list[3][0].BLIT_IMG, flip_list[3][1])
-		SCREEN.blit(flip_list[4][0].BLIT_IMG, flip_list[4][1])
+		#SCREEN.blit(flip_list[0][0].BLIT_IMG, flip_list[0][1])
+		#anim.main((0, 0) ,[flip_list[0][0].BLIT_IMG])
+		#SCREEN.blit(flip_list[1][0].BLIT_IMG, flip_list[1][1])
+		#anim.main((50, 0) ,[flip_list[1][0].BLIT_IMG])
+		#SCREEN.blit(flip_list[2][0].BLIT_IMG, flip_list[2][1])
+		#anim.main((100, 0) ,[flip_list[2][0].BLIT_IMG])
+		#SCREEN.blit(flip_list[3][0].BLIT_IMG, flip_list[3][1])
+		#anim.main((150, 0) ,[flip_list[3][0].BLIT_IMG])
+		#SCREEN.blit(flip_list[4][0].BLIT_IMG, flip_list[4][1])
+		#anim.main((200, 0) ,[flip_list[4][0].BLIT_IMG])
+		#pygame.display.update()
+		#time.sleep(1)
+
 	elif KEY_INPUT:
 		SCREEN.blit(check.BLIT_IMG, (0, 0))
 
